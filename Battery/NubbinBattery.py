3fef4fe4a93dbbdcc59890f6572192a4f480ba6c
saJeelakhan <135961783+saJeelakhan@users.noreply.github.com>
2023-07-07 14:09:48 +0500

    Add files via upload

---
+++ Battery/NubbinBattery.py
@@ -0,0 +1,12 @@
+class NubbinBattery(Battery):
+    
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        
+    def needs_service():
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < current_date:
+            return True
+        else:
+            return False
