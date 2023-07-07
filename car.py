d26eea29e4c041916882dc153450ab3b4a517731
saJeelakhan <135961783+saJeelakhan@users.noreply.github.com>
2023-07-07 18:24:04 +0500

    Update car.py

--- car.py
+++ car.py
@@ -1,8 +1,9 @@
 class Car(Serviceable):
-    def __init__(self, Engine, Battery):
+    def __init__(self, Engine, Battery, tires):
         engine = Engine
         battery = Battery
+        self.tires = tires
         
     @abstractmethod
     def needs_service(self):
-        return engine.needs_service() or battery.needs_service()
+        return engine.needs_service() or battery.needs_service() or self.tires.needs_service()
