4e0182d3be2a42296dbeba2386d13e92dcb60c92
saJeelakhan <135961783+saJeelakhan@users.noreply.github.com>
2023-07-07 14:20:47 +0500

    Car_Models.py

--- Car_Models.py
+++ Car_Models.py
@@ -1,1 +1,43 @@
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
 
+
+class CarFactory:
+    @staticmethod
+    def calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        Car = car(engine, battery)
+        return Car
+
+    @staticmethod
+    def glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        Car = car(engine, battery)
+        return Car
+
+    @staticmethod
+    def palindrome(current_date, last_service_date, warning_light_is_on):
+        engine = SternmanEngine(warning_light_is_on)
+        battery = SpindlerBattery(current_date, last_service_date)
+        Car = car(engine, battery)
+        return Car
+
+    @staticmethod
+    def rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        Car = car(engine, battery)
+        return Car
+
+    @staticmethod
+    def thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        Car = car(engine, battery)
+        return Car
