1d6d49efaf7d3dd74131ce5db3f6762273ddb82b
saJeelakhan <135961783+saJeelakhan@users.noreply.github.com>
2023-07-07 16:27:19 +0500

    Update NubbinBattery.py

--- Battery/NubbinBattery.py
+++ Battery/NubbinBattery.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 class NubbinBattery(Battery):
     
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
         self.last_service_date = last_service_date
         
-    def needs_service():
+    def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
         if service_threshold_date < current_date:
             return True
